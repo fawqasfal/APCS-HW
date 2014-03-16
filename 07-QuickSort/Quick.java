bba566df06f2c7f933372e4a188ce93c1fa618ba
omarhegazy <omarbicboi@gmail.com>
2014-03-15 21:17:12 -0400

    should start using emacs

--- 07-QuickSort/Quick.java
+++ 07-QuickSort/Quick.java
@@ -3,9 +3,10 @@ public class Quick {
 	public static void main(String[] args) {
 		int[] test = new int[10];
 		for (int i = 0; i < 10; i++) 
-			test[i] = (int) (Math.random() * 10);
+			test[i] = 1 + (int) (Math.random() * 11);
+
 		System.out.println(Arrays.toString(test));
-		System.out.println(select(test, 3));
+		sort(test);
 		System.out.println(Arrays.toString(test));
 
 	}
@@ -34,9 +35,9 @@ public class Quick {
 		return wall; //the pivot index has the pivot-th largest # 
 	}
 	private static int select(int[] a, int k, int low, int high) {
+		//trying to find the kth biggest element (a.length -1  = biggest; 0 = smallest)
 		if (a.length == 1)
 			return a[0];
-		//trying to find the kth biggest element (a.length -1  = biggest; 0 = smallest)
 		int pivot = low + (int) (Math.random() * ((high - low) + 1)); //random index - this index will be the correct sorted num  
 		pivot = partition(pivot, a, low, high);
 
@@ -55,10 +56,16 @@ public class Quick {
 			return -1;
 		}
  	}
- 	public static int[] sort (int[] a) {
- 		if (a.length == 1)
- 			return a;
- 		int pivot = 0 + (int) (Math.random() * ((a.length - 0) + 1)); 
-
+ 	private static void sort(int[] a, int low, int high) {
+ 		//sort by recursively partition()ing everything in the array
+ 		if (low >= high) //youve reached an array with size less than or equal to 0 
+ 			return;
+ 		int pivot = low + (int) (Math.random() * ((high - low) + 1));
+ 		pivot = partition(pivot, a, low, high);
+ 		sort(a, low, pivot - 1);
+ 		sort(a, pivot + 1, high);
+ 	}
+ 	public static void sort(int[] a) {
+ 		sort(a, 0, a.length - 1);
  	}
 }
