ef918a0ad33e525812d6db3009ee6c1c9c4d8499
omarhegazy <omarbicboi@gmail.com>
2014-04-06 19:19:21 -0400

    array stack done

---
+++ 12-Stack/MyStack.java
@@ -0,0 +1,58 @@
+public class MyStack {
+
+    private int top;
+    private int numElts;
+    private String[] array;
+    public MyStack(){
+	top = -1;
+	numElts = 0;
+	array = new String[10];
+    }
+
+    public void push(String s){
+    	numElts++;
+		if (top == array.length - 1) {
+			String[] newarr = new String[numElts + 10];
+			for (int i = 0; i < array.length; i++) newarr[i] = array[i];
+		}	
+		top++;
+		this.array[top] = s;
+    }
+
+    public String pop() {
+		if (numElts == 0) return null;
+		String s = array[top];
+		array[top] = null;
+		return s;
+    }
+
+    public String peek() {
+    	if (numElts == 0) return null;
+    	return array[top];
+    }
+    
+    public int size() {
+	return numElts;
+    }
+
+    public boolean isEmpty() {
+	return array[top]==null;
+    }
+
+    public String toString() {
+	String s="";
+	
+	for (int i = 0; i < array.length && array[i] != null; i++) {
+		s += array[i] + ", ";
+	}
+	/*
+	Node tmp = top;
+	while(tmp!=null) {
+	    s = s + tmp.getData()+", ";
+	    tmp = tmp.getNext();
+	}
+	*/
+	return s.substring(0,s.length()-2);
+    }
+    
+}
