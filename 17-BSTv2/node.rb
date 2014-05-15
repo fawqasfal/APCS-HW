1fcfa272f8c72b1664bd6dd8a681625c14ab1377
omarhegazy <omarbicboi@gmail.com>
2014-05-15 00:04:21 -0400

    heap insert done.

--- 17-BSTv2/node.rb
+++ 17-BSTv2/node.rb
@@ -5,12 +5,13 @@
 # Node initialize(left = nil, right = nil, data) -> constructor. left/right must be Nodes.
 # void left=(node) -> sets left child. left child is either nil or a Node whose data is of the same type as self.data's.
 # void right=(node) -> sets right child. same rules as left=.
+# void data=(dt) -> sets data to dt. If left and/or right exist, must be same type as either/both, else raises ArgumentError.
 # string inspect -> gets datas inspect			 
 
 
 class Node
-  attr_reader :left, :right
-  attr_accessor :data
+  attr_reader :left, :right, :data
+
   def initialize(data, left: nil, right: nil)
   	@left = left
   	@right = right
@@ -33,6 +34,15 @@ class Node
   	raise(ArgumentError, not_even_node) unless newnode.class == Node or newnode.nil?
   	@right = newnode
   end
+
+  def data=(dt) 
+    dt_break_l = "You tried giving node #{inspect} data #{dt.inspect} -- left child data type is #{left.data.class}." if not left.nil?
+    dt_break_r = "You tried giving node #{inspect} data #{dt.inspect} -- right child data type is #{right.data.class}." if not right.nil?
+    raise(ArgumentError, dt_break_l) if not dt_break_l.nil? and left.data.class != dt.class
+    raise(ArgumentError,dt_break_r) if not dt_break_r.nil? and right.data.class != dt.class
+    @data = dt 
+  end
+
 
   def nil?; return @data == nil; end
   def inspect;to_s;end
